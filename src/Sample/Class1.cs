7bc139ce1906f65f3eef94ac46e6643b7757d219
SeungYongShim <syshim@me.com>
2022-06-12 08:57:19 +0900

    .

--- src/Sample/Class1.cs
+++ src/Sample/Class1.cs
@@ -5,10 +5,11 @@ using RT = LanguageExt.Sys.Live.Runtime;
 Error Failed = "I asked you to say hello, and you can't even do that?!";
 
 var q = (from _ in Console<RT>.writeLine("Say hello")
-         from t in Console<RT>.readLine
+         from t in Console.In.ReadLineAsync().ToAff()
          from e in guard(t == "hello", Failed)
          from m in Console<RT>.writeLine("Hi")
-         select unit).ToAff().Retry(Schedule.Recurs(5));
+         select unit).Retry(Schedule.recurs(5));
 
 await q.Run(RT.New());
+
 
