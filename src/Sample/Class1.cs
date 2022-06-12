4c5effb9f401bf767e42c5a80b81e93bcc2a3b6b
SeungYongShim <syshim@me.com>
2022-06-12 10:19:01 +0900

    .

--- src/Sample/Class1.cs
+++ src/Sample/Class1.cs
@@ -1,15 +1,27 @@
+using Cff.Effect;
+using Cff.Effect.Abstractions;
+using Cff.Effect.Json;
+using Cff.Effect.Sha;
 using LanguageExt.Common;
-using LanguageExt.Sys;
-using RT = LanguageExt.Sys.Live.Runtime;
 
 Error Failed = "I asked you to say hello, and you can't even do that?!";
 
-var q = (from _ in Console<RT>.writeLine("Say hello")
-         from t in Console.In.ReadLineAsync().ToAff()
-         from e in guard(t == "hello", Failed)
-         from m in Console<RT>.writeLine("Hi")
-         select unit).Retry(Schedule.recurs(5));
+using var ms = new MemoryStream("Hello\nHello\nHello\nnHello\nnHello\nnHello\nnHello\nhello\n".ToCharArray().Select(x => (byte)x).ToArray());
+using var sr = new StreamReader(ms);
 
-await q.Run(RT.New());
+var q = (from __ in unitAff
+         from _1 in Console.Out.WriteLineAsync("Say hello").ToUnit().ToAff()
+         from _2 in sr.ReadLineAsync().ToAff()
+         from _3 in Sha<RT>.SerializeEff(_2)
+         from _4 in Console.Out.WriteLineAsync(_3.ToString()).ToUnit().ToAff()
+         from _5 in guard(_2 == "hello", Failed)
+         from _6 in Console.Out.WriteLineAsync("Hi").ToUnit().ToAff()
+         select unit).Retry(Schedule.recurs(10));
 
+using var ct = new CancellationTokenSource();
+await q.Run(new RT(ct));
 
+public readonly record struct RT(CancellationTokenSource CancellationTokenSource)
+    : HasCancelDefault<RT>,
+      HasJson<RT>,
+      HasSha512<RT>;
